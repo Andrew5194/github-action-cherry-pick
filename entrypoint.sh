b07e78dafddbde8f597f4e7c95c44c2e5c872ac4
Andrew Yang <andrew.yang5194@gmail.com>
2023-07-07 16:42:17 -0400

    adding if-else statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -38,9 +38,14 @@ PR_TITLE=$(git log -1 --format="%s" $GITHUB_SHA)
 PR_NUMBER="${PR_TITLE#*#}"    # Remove everything before the '#' character
 PR_NUMBER="${PR_NUMBER%% *}"  # Remove everything after the first space character
 
-git_cmd git remote update
-git_cmd git fetch --all
-git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
-git_cmd gh pr diff --patch $PR_NUMBER | git am
-git_cmd git push -u origin "${PR_BRANCH}"
-git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
+if [ -n "$PR_NUMBER" ]; then
+  echo "PR number found. Creating cherry pick PR."
+  git_cmd git remote update
+  git_cmd git fetch --all
+  git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
+  git_cmd gh pr diff --patch $PR_NUMBER | git am
+  git_cmd git push -u origin "${PR_BRANCH}"
+  git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
+else
+  echo "PR number not found. Aborting."
+fi
