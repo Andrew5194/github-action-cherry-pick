056fbedd1324c296531db0920238f076a4d253f2
Andrew Yang <andrew.yang5194@gmail.com>
2023-07-07 16:19:53 -0400

    cleanning up

--- test.sh
+++
@@ -1,5 +0,0 @@
-set -x
-
-string="Merge pull request #7 from Andrew5194/testing-cherry-pick-action"
-pr_number=$(echo "$string" | grep -oP '(?<=Merge pull request #)[0-9]+')
-echo "PR Number: $pr_number"
