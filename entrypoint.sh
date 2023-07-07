25fb8eb1417dc3a98ef3eaf5585050146e8fb957
Andrew Yang <andrew.yang5194@gmail.com>
2023-07-07 15:29:34 -0400

    adding gh pr list

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,7 +39,8 @@ git_setup
 git_cmd git remote update
 git_cmd git fetch --all
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
-git_cmd gh pr diff --patch 5 | git am
+git_cmd gh pr list
+# git_cmd gh pr diff --patch 5 | git am
 # git_cmd git cherry-pick "${GITHUB_SHA}"
-git_cmd git push -u origin "${PR_BRANCH}"
-git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
+# git_cmd git push -u origin "${PR_BRANCH}"
+# git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
