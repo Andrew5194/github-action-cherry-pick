a422caf70e8d206c1780d921dd7a4c48e0b45249
Andrew Yang <andrew.yang5194@gmail.com>
2023-07-07 17:11:18 -0400

    adding checks for PR merge to main vs. direct merge to main

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,14 +42,17 @@ if expr index "$PR_TITLE" "#" > /dev/null; then
   PR_NUMBER=$(echo "$PR_TITLE" | sed 's/.*#\([0-9]\{1,\}\).*/\1/')
 fi
 
+git_cmd git remote update
+git_cmd git fetch --all
+git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
+
 if [ -n "$PR_NUMBER" ]; then
   echo "PR number found. Creating cherry pick PR."
-  git_cmd git remote update
-  git_cmd git fetch --all
-  git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
   git_cmd gh pr diff --patch $PR_NUMBER | git am
-  git_cmd git push -u origin "${PR_BRANCH}"
-  git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO 🍒: ${PR_TITLE}\""
 else
-  echo "PR number not found. Aborting."
+  echo "PR number not found. Creating cherry pick PR for direct commit."
+  git_cmd git cherry-pick "${GITHUB_SHA}"
 fi
+
+git_cmd git push -u origin "${PR_BRANCH}"
+git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO 🍒: ${PR_TITLE}\""
